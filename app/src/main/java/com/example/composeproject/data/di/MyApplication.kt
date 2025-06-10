01b8de1092355cf1614c7556a4b8d7f534cd21f4
Vijaykumar Lande <vijaykumar70287606@gmail.com>
2025-06-11 00:15:52 +0530

    Provide HiltWorkerFactory to use hilt with WorkManager

--- app/src/main/java/com/example/composeproject/data/di/MyApplication.kt
+++ app/src/main/java/com/example/composeproject/data/di/MyApplication.kt
@@ -1,10 +1,25 @@
 package com.example.composeproject.data.di
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+//@HiltAndroidApp
+//class MyApplication: Application()
+////trigger Hilt's code generation and set up the dependency container at the application level.
 
 @HiltAndroidApp
-class MyApplication: Application()
-//trigger Hilt's code generation and set up the dependency container at the application level.
+class MyApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
--- app/src/main/java/com/example/composeproject/task/ReminderWorker.kt
+++ app/src/main/java/com/example/composeproject/task/ReminderWorker.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.example.composeproject.data.repository.NotificationRepository
